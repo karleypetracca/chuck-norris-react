4c5da37f5376345f0a6763cdeecfb6bd361c1b9e
Karley Petracca <karleypetracca@gmail.com>
2020-03-24 14:30:29 -0400

    replaced prop variable with state localized in class

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
-class Quote extends Component {    
+class Quote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "hahahaha",
+            message2: "charcuterie rulez",
+        }
+    }
+
     render() {
-        const { message } = this.props; //destructured
+        const { message, message2 } = this.state; //destructured
         return (
             <div>
-                <p>Chuck says... {message} </p>
+                <p>Chuck says... {message2} </p>
             </div>
         )
     }
