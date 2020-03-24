31097ce06be4f56ded5774a15984d996d21f2e29
Karley Petracca <karleypetracca@gmail.com>
2020-03-24 13:50:58 -0400

    cleaned up source template code and added first class for quote

---
+++ src/components/quote.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+    render() {
+        return (
+            <div>
+                <p>Chuck says...</p>
+            </div>
+        )
+    }
+}
+
+export default Quote;
